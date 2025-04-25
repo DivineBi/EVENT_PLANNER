ae40f2e4ed01247895ac0199c650cfd70fbda188
Divine-Bi <dbingila@outlook.com>
2025-04-25 17:52:00 +0200

    added description, categories, contact form  and the footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+        return (
+    <>
+        <section>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+        </section>
+    </>
+
+    );
+};
+
+export default Footer;
